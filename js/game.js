f037cdad86ee92f557e99c5a7fe721fc92939b15
Alexander <mrreads@yandex.com>
2020-01-05 03:40:40 +0300

    data to json

--- js/game.js
+++ js/game.js
@@ -1,30 +1,58 @@
 ﻿class Game
 {
-    dialog(author, character, text, background)
+    screen(type, author, character, text, background)
     {
-        let screen = document.querySelector('#screen');
-        screen.innerHTML = '';
-        screen.style.backgroundImage = 'url("'+background+'")';
 
-        let messageElement = document.createElement("div");
-        messageElement.classList.add('message');
-        screen.appendChild(messageElement);
+        if (type == 'dialog')
+        {
+            let screen = document.querySelector('#screen');
+            screen.innerHTML = '';
+            screen.style.backgroundImage = 'url("'+background+'")';
 
-        let authorElement = document.createElement("p");
-        authorElement.classList.add('author');
-        authorElement.textContent = author;
-        messageElement.appendChild(authorElement);
+            let messageElement = document.createElement("div");
+            messageElement.classList.add('message');
+            screen.appendChild(messageElement);
 
-        let characterElement = document.createElement("img");
-        characterElement.classList.add('character');
-        characterElement.src = character;
-        messageElement.appendChild(characterElement);
+            let authorElement = document.createElement("p");
+            authorElement.classList.add('author');
+            authorElement.textContent = author;
+            messageElement.appendChild(authorElement);
 
-        let textElement = document.createElement("p");
-        textElement.classList.add('text');
-        textElement.textContent = text;
-        messageElement.appendChild(textElement);
+            let characterElement = document.createElement("img");
+            characterElement.classList.add('character');
+            characterElement.src = character;
+            messageElement.appendChild(characterElement);
+
+            let textElement = document.createElement("p");
+            textElement.classList.add('text');
+            textElement.textContent = text;
+            messageElement.appendChild(textElement);
+        }
     }
 }
 
 var game = new Game();
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', './../resources/data/game.json', true);
+xhr.send();
+xhr.onload = () => 
+{
+    let type;
+    let author;
+    let character;
+    let background;
+    let text;
+
+    let gameData = JSON.parse(xhr.response);
+    gameData.forEach((act) => 
+    {
+        type = act['type'];
+        author = act['author'];
+        character = act['character'];
+        background = act['background'];
+        text = act['text'];
+
+        game.screen(type, author, character, text, background);
+    });
+}
